ebc33b768cdbab2d21f1335edf85cf3e3554e277
Diana Belokon <67607236+dbelokon@users.noreply.github.com>
2021-11-14 00:03:47 -0500

    Add unit tests and update contributing.md

---
+++ glazed-donut.Tests/RawTextParserTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+using GlazedDonut.FileParsing.Parsing;
+
+namespace GlazedDonut.Tests
+{
+    public class RawTextParserTests
+    {
+        [Fact]
+        public void Parse_InputIsMemoryStream_ReturnHtmlString()
+        {
+            var str = "Hello, this is raw text.";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(str));
+            var parser = new RawTextParser(stream);
+
+            var htmlString = parser.Parse();
+
+            Assert.Equal($"<p>{str}</p>\n", htmlString);
+        }
+
+        [Fact]
+        public void Parse_InputIsEmpty_ReturnEmptyString()
+        {
+            var str = "";
+            var stream = new MemoryStream(Encoding.UTF8.GetBytes(str));
+            var parser = new RawTextParser(stream);
+
+            var htmlString = parser.Parse();
+
+            Assert.Equal("", htmlString);
+        }
+
+        [Fact]
+        public void Constructor_InputIsNull_ShouldThrowArgumentNullException()
+        {
+            ArgumentNullException e = Assert.Throws<ArgumentNullException>(() => {
+                var parser = new RawTextParser(null);
+            });
+
+            Assert.NotNull(e);
+        }
+    }
+}
